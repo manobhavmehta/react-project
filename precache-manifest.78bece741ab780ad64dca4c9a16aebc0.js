605e365ec3a15ddf05cc74ec99bdcbd4d85d19b2
manobhavmehta <manobhavmehta88@gmail.com>
2023-05-26 09:54:50 +0530

    Updates

---
+++ precache-manifest.78bece741ab780ad64dca4c9a16aebc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb528d4d6a4474040100",
+    "url": "/react-project/static/css/main.11bf3fb6.chunk.css"
+  },
+  {
+    "revision": "fb528d4d6a4474040100",
+    "url": "/react-project/static/js/main.fb528d4d.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/react-project/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "6b4ea7df01b11a6c2678",
+    "url": "/react-project/static/js/2.6b4ea7df.chunk.js"
+  },
+  {
+    "revision": "8794345dc8948653dbb7",
+    "url": "/react-project/static/js/runtime~main.8794345d.js"
+  },
+  {
+    "revision": "531a1a27972dc5d1d0b053e368d6e643",
+    "url": "/react-project/index.html"
+  }
+];
